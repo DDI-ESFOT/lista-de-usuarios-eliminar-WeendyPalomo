e432e2a0cbed9736609eb367fef62d08f2b1e699
WeendyPalomo <58180852+WeendyPalomo@users.noreply.github.com>
2021-02-23 00:00:08 -0500

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,6 +1,3 @@
-/**
- * Created by chalosalvador on 4/2/21
- */
 import { useState } from "react";
 
 function formatName(user) {
@@ -23,27 +20,43 @@ const UserList = ({ users }) => {
 
     setUsersList((prevUsersList) => {
       // Spread operator
+      //  SIEMPRE
+      // la funcion setXXX debe retornar algo (la misma lista talvez)
+      // las finciones set debe retornar el nuevo valor
       return [...prevUsersList, newUser];
     });
 
+    //vaciar el input despues de agregar un usuario
     document.querySelector("#name").value = "";
     document.querySelector("#lastname").value = "";
   };
 
+  const handleDeleteUser = (event) => {
+    console.log("Eliminar");
+    setUsersList((prevUsersList) => {
+      prevUsersList.pop();
+      return [...prevUsersList];
+    });
+  }
+
+
   return (
     <>
       <h1>Lista de usuarios ({usersList.length} usuarios)</h1>
+      
       <input type="text" id="name" placeholder="Ingrese un nombre" />
       <input type="text" id="lastname" placeholder="Ingrese un apellido" />
       <button onClick={handleAddUser}>Añadir</button>
-      <button>Eliminar el último usuario</button>
+      <button onClick={handleDeleteUser}>Eliminar el último usuario</button>
       <ul>
-        {usersList.map((user, index) => {
+        {usersList.map((user) => {
           return <li key={Math.random()}>{formatName(user)}</li>;
         })}
       </ul>
     </>
   );
+
+
 };
 
 export default UserList;
